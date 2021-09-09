d979e527ab06a9e98d1bbcad4c625ac80f4609e4
Morteza Jamali <mortezajamali4241@gmail.com>
2021-09-10 00:09:25 +0430

    feat: 'log_msg()' function created

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -4,34 +4,35 @@ NVM_INSTALL_URL=""
 DENO_INSTALL_URL=""
 WORKSPACE_URL=""
 UPGRADE_SYS_LIBS=0
-SHOW_LOG=1
+
+# Script options
+NVM_INSTALL_OPTION="--nvm-url"
+DENO_INSTALL_OPTION="--deno-url"
+WORKSPACE_CLONE_OPTION="--workspace-url"
+NO_UPGRADE_OPTION="--no-upgrade"
 
 tty -s
 
 # Run option commands
 while [ "$#" -gt 0 ]; do
-  case "$1" in
-    --nvm-url)
+  case "${1^^}" in
+    "${NVM_INSTALL_OPTION^^}")
       NVM_INSTALL_URL="$2"
       shift
       shift
     ;;
-    --deno-url)
+    "${DENO_INSTALL_OPTION^^}")
       DENO_INSTALL_URL="$2"
       shift
       shift
     ;;
-    --workspace-url)
+    "${WORKSPACE_CLONE_OPTION^^}")
       WORKSPACE_URL="$2"
       shift
       shift
     ;;
-    --no-upgrade)
+    "${NO_UPGRADE_OPTION^^}")
       UPGRADE_SYS_LIBS=1
-      shift
-    ;;
-    --show-log)
-      SHOW_LOG=0
       shift
     ;;
     *)
@@ -40,18 +41,67 @@ while [ "$#" -gt 0 ]; do
   esac
 done
 
-# Toggle log display
-if [ $SHOW_LOG = 1 ]; then
-  exec 3>&1 &>/dev/null
-else
-  exec 3>&1
-fi
+# Hide logs
+exec 3>&1 &>/dev/null
 
-if [ -z "$NVM_INSTALL_URL" ] || \
-   [ -z "$DENO_INSTALL_URL" ] || \
-   [ -z "$WORKSPACE_URL" ]; then
-  echo "ERROR:: Arguments are not set properly !"
-  exit 1
+# Log messages
+log_msg() {
+  local type=${2:-"-d"}
+  local reset="\033[0m"
+  local color="\033"
+
+  case ${type^^} in
+    -E)
+      color="$color[31m"
+    ;;
+    -S)
+      color="$color[32m"
+    ;;
+    -D)
+      color=$reset
+    ;;
+  esac
+
+  echo -e "${color}$1$reset" >&3
+}
+
+log_error() {
+  log_msg "ERROR:: $1" -e
+
+  if [[ ${2^^} = "-E" ]]; then
+    exit 1
+  fi
+}
+
+box_drawer() {
+  local s=("$@") b w
+
+  for l in "${s[@]}"; do
+    ((w<${#l})) && { b="$l"; w="${#l}"; }
+  done
+
+  tput setaf 3
+
+  echo " -${b//?/-}-
+| ${b//?/ } |" >&3
+
+  for l in "${s[@]}"; do
+    printf '| %s%*s%s |\n' "$(tput setaf 4)" "-$w" "$l" "$(tput setaf 3)" >&3
+  done
+
+  echo "| ${b//?/ } |
+ -${b//?/-}-" >&3
+
+  tput sgr 0
+}
+
+# Check options are set properly
+if [ -z "$NVM_INSTALL_URL" ]; then
+  log_error "Option '$NVM_INSTALL_OPTION' is not set !" -e
+elif [ -z "$DENO_INSTALL_URL" ]; then
+  log_error "Option '$DENO_INSTALL_OPTION' is not set !" -e
+elif [ -z "$WORKSPACE_URL" ]; then
+  log_error "Option '$WORKSPACE_CLONE_OPTION' is not set !" -e
 fi
 
 # Run commands using SSH
