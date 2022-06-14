6f5c9565567602a4e1d551e80950b59acfa9603d
Morteza Jamali <mortezajamali4241@gmail.com>
2022-06-14 04:34:56 +0000

    fix(packages): check the installation status of libraries in `provision/basic_tools.sh`

--- packages/provision/basic_tools.sh
+++ packages/provision/basic_tools.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt update
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt install \
   unzip \
   zip \
@@ -11,35 +14,55 @@ sudo apt install \
   -y
 
 # sdkman
-curl -s "https://get.sdkman.io" | bash
-source "/home/vagrant/.sdkman/bin/sdkman-init.sh"
+if $(command -v sdk &>/dev/null && echo false); then
+  clear
+  echo '============= Install sdkman ==========='
+  #curl -s "https://get.sdkman.io" | bash
+  #source "/home/vagrant/.sdkman/bin/sdkman-init.sh"
+fi
 
 # docker engine
-sudo apt-get install \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release \
-  -y
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
- "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
- $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-compose-plugin \
-  -y
+if ! command -v docker &>/dev/null; then
+  sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    -y
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-compose-plugin \
+    -y
+fi
 
 # fnm, nodejs, npm, yarn
-curl -fsSL https://fnm.vercel.app/install | bash
-source ~/.bashrc
-fnm install --lts
-npm i -g npm@latest
-npm i -g yarn
+if ! command -v fnm &>/dev/null; then
+  curl -fsSL https://fnm.vercel.app/install | bash
+fi
+
+. ~/.bashrc
+
+if ! command -v node &>/dev/null; then
+  fnm install --lts
+  npm i -g npm@latest
+fi
+
+if ! command -v yarn &>/dev/null; then
+  npm i -g yarn
+fi
 
 # act
-curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+if ! command -v act &>/dev/null; then
+  curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+  mkdir -p .act/bin
+  sudo mv bin/act .act/bin/
+  sudo rm -rf bin
+fi
