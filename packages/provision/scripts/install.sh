64413903543c7c2769689607d93581b88ec006d2
Morteza Jamali <mortezajamali4241@gmail.com>
2022-06-27 08:27:07 +0000

    fix(packages): fix `bpm` binary in `provision/scripts/install.sh` `#2`

--- packages/provision/scripts/install.sh
+++ packages/provision/scripts/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+shopt -s expand_aliases
+
 # Install bpm
 if ! command -v bpm &>/dev/null; then
   curl -fsSL https://raw.githubusercontent.com/morajlab/bpm/master/packages/install/install.sh | bash
@@ -7,5 +9,7 @@ else
   echo 'bpm already installed !'
 fi
 
+alias bpm=$HOME/.bpm/bpm
+
 # TODO: Add this feature to bpm installer script
-$HOME/.bpm/bpm install is_installed
+bpm install is_installed
