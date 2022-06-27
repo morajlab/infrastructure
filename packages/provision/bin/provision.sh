4848df15192e2f3cad47ab8ff1cec32edcbd289d
Morteza Jamali <mortezajamali4241@gmail.com>
2022-06-27 09:47:53 +0000

    chore(base): improve `wsl.yaml` workflow

--- packages/provision/bin/provision.sh
+++ packages/provision/bin/provision.sh
@@ -18,4 +18,4 @@ fi
 
 alias ansible-playbook="$HOME/.local/bin/ansible-playbook"
 
-ansible-playbook $PROVISION_PATH/playbooks/playbook.yml -K
+ansible-playbook $PROVISION_PATH/playbooks/playbook.yml -K -v
