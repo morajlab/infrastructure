444d673d677340eae0b4c2eb5d774554ab96503d
Morteza Jamali <mortezajamali4241@gmail.com>
2022-07-20 03:15:46 +0000

    chore(scripts): update workflow scripts

--- scripts/workflow/get_obj_url.ps1
+++ scripts/workflow/get_obj_url.ps1
@@ -1,5 +1,5 @@
 $object = $args[0]
 
-& "$env:USERPROFILE\uplink_windows_amd64\uplink.exe share $object --url --not-after=none" | select -Last 1 | ForEach-Object {
+& "$env:USERPROFILE\uplink_windows_amd64\uplink.exe" share $object --url --not-after=none | select -Last 1 | ForEach-Object {
     $_.split(" ")[-1]
 }
