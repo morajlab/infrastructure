cb0c50e4ef7a12fc1ebf1e0e3508fa10e0ca57b0
Morteza Jamali <mortezajamali4241@gmail.com>
2022-10-27 20:06:48 +0000

    chore(scripts): create `Test-Arguments` function in `provision_host.ps1` script

--- scripts/provision_host.ps1
+++ scripts/provision_host.ps1
@@ -1,4 +1,61 @@
-function Set-EnvironmentVariables {    
+# TODO: Complete the list of providers
+$PROVIDERS_LIST = @{
+    VIRTUALBOX = "virtualbox";
+};
+
+function Test-RootPath {
+    return $true;
+}
+
+function Test-DefaultProvider {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $DefaultProvider
+    )
+
+    $PROVIDERS_LIST.Values | ForEach-Object {
+        if ($DefaultProvider -ceq $_) {
+            return $true;            
+        }
+    }
+
+    return $false;
+}
+
+function Test-Arguments {
+    [CmdletBinding()]
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $RootPath = "C:\Vagrant-Workspaces", # Get system drive name
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $DefaultProvider = $PROVIDERS_LIST.VIRTUALBOX
+    )
+
+    if (-Not (Test-DefaultProvider -DefaultProvider $DefaultProvider)) {
+        $exception = [System.Exception]@{
+            Source   = "provision_host.ps1";
+            HelpLink = "https://developer.hashicorp.com/vagrant/docs/providers"
+        };
+
+        Write-Error `
+            -Message "Invalid value" `
+            -Category InvalidArgument `
+            -TargetObject $DefaultProvider `
+            -Exception $exception;
+    
+        Exit 1;
+    }
+    
+    # if (-Not (Test-RootPath)) {}
+}
+
+Test-Arguments @args
+
+function Set-EnvironmentVariables {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -12,10 +69,11 @@ function Set-EnvironmentVariables {
     }
 }
 
-$envariables = @{
-    VAGRANT_HOME                   = "E:\.vagrant.d"
-    VAGRANT_DEFAULT_PROVIDER       = "virtualbox"
-    VAGRANT_VMWARE_CLONE_DIRECTORY = "E:\virtual-machines\vmware"
-};
+# $envariables = @{
+#     VAGRANT_HOME                   = Join-Path $RootPath ".vagrant.d"
+#     VAGRANT_VAGRANTFILE            = "Vagrantfile.rb"
+#     VAGRANT_DEFAULT_PROVIDER       = "virtualbox"
+#     VAGRANT_VMWARE_CLONE_DIRECTORY = Join-Path $RootPath "virtual-machines\vmware"
+# };
 
-Set-EnvironmentVariables -Variables $envariables -Debug
+# Set-EnvironmentVariables -Variables $envariables -Debug:$debug
