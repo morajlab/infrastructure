63be6be95261f9e6dd548413e3c5db2ca6444fa9
morteza-jamali <mortezajamali4241@gmail.com>
2023-01-12 14:44:26 +0330

    chore(scripts): create `Tasks List` component in `install.js` script

--- scripts/install.js
+++ scripts/install.js
@@ -1,11 +1,36 @@
-const { URL } = require('url');
+const React = require('react');
 const Husky = require('husky');
+const spinners = require('cli-spinners');
+const crc = require('create-react-class');
+const { URL } = require('url');
+const { TaskList, Task } = require('ink-task-list');
 const { simpleGit } = require('simple-git');
+const { render } = require('ink');
+
 const mjwConfig = require('../mjw.config.json');
 
-Husky.install();
+const TASK_STATES = {
+  ERROR: 'error',
+  SUCCESS: 'success',
+  PENDING: 'pending',
+  LOADING: 'loading',
+};
+const ce = React.createElement;
 
-function install_submodules() {
+function installHusky() {
+  const promise = new Promise((resolve, _reject) => {
+    const log = console.log;
+    console.log = () => {};
+
+    Husky.install();
+    console.log = log;
+    resolve();
+  });
+
+  return [promise];
+}
+
+function installSubmodules() {
   const submodules = mjwConfig?.git?.submodules;
 
   if (!submodules || !submodules?.modules) {
@@ -13,10 +38,9 @@ function install_submodules() {
   }
 
   const options = submodules.options ?? {};
-
-  simpleGit().submoduleUpdate(['--init', '--recursive']);
-
-  const modules_promises = [];
+  const promises_array = [
+    simpleGit().submoduleUpdate(['--init', '--recursive']),
+  ];
 
   submodules.modules.forEach((m) => {
     const branch = m.branch ?? options.default_branch;
@@ -32,12 +56,52 @@ function install_submodules() {
       .replace(/\.git$/, '');
     const module_path = (options.root_path ?? '') + '/' + (m.path ?? repo_name);
 
-    modules_promises.push(simpleGit(module_path).checkout(branch));
+    promises_array.push(simpleGit(module_path).checkout(branch));
   });
 
-  Promise.allSettled(modules_promises).then((results) =>
-    results.forEach((result) => console.log(result))
-  );
+  return promises_array;
 }
 
-install_submodules();
+const tasks = [
+  {
+    label: 'Install Husky',
+    func: installHusky,
+  },
+  {
+    label: 'Install Submodules',
+    func: installSubmodules,
+  },
+];
+
+const TasksComponents = [];
+
+tasks.forEach((task, key) => {
+  const component = crc({
+    getInitialState: function () {
+      return { state: TASK_STATES.LOADING };
+    },
+    componentDidMount: function () {
+      Promise.all(task.func())
+        .then(() => {
+          this.setState({ state: TASK_STATES.SUCCESS });
+        })
+        .catch(() => {
+          this.setState({ state: TASK_STATES.ERROR });
+        });
+    },
+    render: function () {
+      const label =
+        task.label ?? task.func.name.replaceAll('_', ' ').toUpperCase();
+
+      return ce(Task, {
+        label,
+        state: this.state.state,
+        spinner: this.state.state == TASK_STATES.LOADING ? spinners.dots : null,
+      });
+    },
+  });
+
+  TasksComponents.push(ce(component, { key }));
+});
+
+render(ce(TaskList, null, TasksComponents));
